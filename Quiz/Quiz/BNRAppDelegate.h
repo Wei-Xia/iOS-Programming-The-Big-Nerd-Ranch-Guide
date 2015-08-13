385e28a198feee8cd5624144bda4e7c8daa60016
Wei Xia <xw900812@gmail.com>
2015-08-12 23:41:22 -0400

    Chapter 1

---
+++ Quiz/Quiz/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Quiz
+//
+//  Created by Wei on 8/12/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
