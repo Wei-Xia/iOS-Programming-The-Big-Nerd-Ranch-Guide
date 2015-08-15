a39cb0fd753339e7f20b51081777a5f592572035
Wei Xia <xw900812@gmail.com>
2015-08-15 00:45:07 -0400

    Chapter 2

--- RandomItems/RandomItems/BNRItem.h
+++ RandomItems/RandomItems/BNRItem.h
@@ -16,4 +16,17 @@
     int _valueInDollars;
     NSDate *_dateCreated;
 }
+
+-(void)setItemName: (NSString *)str;
+-(NSString *)itemName;
+
+-(void)setSerialNumber: (NSString *)str;
+-(NSString *)serialNumber;
+
+-(void)setValueInDollars: (int)v;
+-(int)valueInDollars;
+
+-(NSDate *)dateCreated;
+
+
 @end
