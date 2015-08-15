b697bd2aeddde468c4f5849a200ae83e6b8d73c6
Wei Xia <xw900812@gmail.com>
2015-08-14 23:31:38 -0400

    Chapter 2

---
+++ RandomItems/RandomItems/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Wei on 8/14/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
