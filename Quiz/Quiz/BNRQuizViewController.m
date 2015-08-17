cd163f016f394cb44c23f712722eb412649f4ced
Wei Xia <xw900812@gmail.com>
2015-08-16 23:20:38 -0400

    Chapter 1

--- Quiz/Quiz/BNRQuizViewController.m
+++ Quiz/Quiz/BNRQuizViewController.m
@@ -22,6 +22,28 @@
 
 @implementation BNRQuizViewController
 
+- (instancetype) initWithNibName:(NSString *)nibNameOrNil
+                          bundle:(NSBundle *)nibBundleOrNil
+{
+    // Call the init method implemented by the superclass
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if (self) {
+        // Create two arrays filled with questions and answers and make the pointers point to them
+        self.questions = @[@"From what is cognac made?",
+                           @"What is 7+7?",
+                           @"What is the capital of Vermont?"];
+        
+        self.answers = @[@"Grapes",
+                         @"14"
+                         @"Montpelier"];
+        
+    }
+    
+    // Return the address of the new object
+    return self;
+}
+
 - (IBAction)showQuestion:(id)sender
 {
     
