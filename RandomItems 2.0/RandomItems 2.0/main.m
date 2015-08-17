d62e283901617b98b93100866e47cb1a0348e916
Wei Xia <xw900812@gmail.com>
2015-08-17 11:57:58 -0400

    Chapter 2

--- RandomItems 2.0/RandomItems 2.0/main.m
+++ RandomItems 2.0/RandomItems 2.0/main.m
@@ -26,39 +26,17 @@ int main(int argc, const char * argv[])
         [items insertObject:@"Zero" atIndex:0];
         
         
-        //        // Iterating over an array
-        //        // For every item in the items array
-        //        for (NSString *item in items) {
-        //            // Log the description of item
-        //            NSLog(@"%@", items);
-        //        }
         
         for (int i = 0; i<[items count]; i++) {
             NSString *item = [items objectAtIndex:i];
             NSLog(@"%@",item);
         }
         
-        //        BNRItem *item = [[BNRItem alloc] init];
-        //
-        //        // This creates an NSString, "Red Sofa" and gives it to the BNRItem
-        ////        [item setItemName:@"Red Sofa"];
-        //        item.itemName = @"Red Sofa";
-        //
-        //        // This creates an NSString, "A1B2C" and gives it to the BNRItem
-        ////        [item setSerialNumber:@"A1B2C"];
-        //        item.serialNumber = @"A1B2C";
-        //
-        //        // This sends the value 100 to be used as the valueInDollars of this BNRItem
-        ////        [item setValueInDollars:100];
-        //        item.valueInDollars = 100;
         
         BNRItem *item = [[BNRItem alloc]initWithItemName:@"Red Sofa"
                                           valueInDollars:100
                                             serialNumber:@"A1B2C"];
-        
-        //        NSLog(@"%@ %@ %@ %d",[item itemName], [item dateCreated], [item serialNumber], [item valueInDollars]);
-        //        NSLog(@"%@, %@, %@, %d", item.itemName, item.dateCreated, item.serialNumber, item.valueInDollars);
-        
+
         // The %@ token is replaced with the result of sending the description message to the corresponding argument
         NSLog(@"%@",item);
         
