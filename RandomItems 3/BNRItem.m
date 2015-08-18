66c758175927f657aadfa137062ce52653e57d43
Wei Xia <xw900812@gmail.com>
2015-08-18 15:47:50 -0400

    Chapter 3

---
+++ RandomItems 3/BNRItem.m
@@ -0,0 +1,129 @@
+//
+//  BNRItem.m
+//  RandomItems 3
+//
+//  Created by Wei on 8/18/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRItem.h"
+
+@implementation BNRItem
+
++ (instancetype)randomItem
+{
+    // Create an immutable array of three adjectives
+    NSArray *randomAdjectiveList = @[@"Fluffy", @"Rusty", @"Shiny"];
+    
+    // Create an immutable array of three nouns
+    NSArray *randomNounList = @[@"Bear", @"Spork", @"Mac"];
+    
+    // Get the index of a random adjective/noun from the lists
+    // Note: the % operator, called the modulo operator, gives you the remainder.
+    // So adjectiveIndex is a random number from 0 to 2 inclusive.
+    // Note that NSInteger is not an object, but a type definition for "Long"
+    NSInteger adjectiveIndex = arc4random() % [randomAdjectiveList count];
+    NSInteger nounIndex = arc4random() % [randomNounList count];
+    
+    NSString *randomName = [NSString stringWithFormat:@"%@, %@",
+                            randomAdjectiveList [adjectiveIndex],
+                            randomNounList [nounIndex]];
+    
+    int randomValue = arc4random() % 100;
+    
+    NSString *randomSerialNumber = [NSString stringWithFormat:@"%c%c%c%c%c",
+                                    '0' + arc4random() %10,
+                                    'A' + arc4random() %26,
+                                    '0' + arc4random() %10,
+                                    'A' + arc4random() %26,
+                                    '0' + arc4random() %10];
+    BNRItem *newItem = [[self alloc]initWithItemName:randomName
+                                      valueInDollars:randomValue
+                                        serialNumber:randomSerialNumber];
+    
+    return newItem;
+    
+}
+
+- (instancetype)initWithItemName:(NSString *)name
+                  valueInDollars:(int)value
+                    serialNumber:(NSString *)sNumber
+{
+    // Call the superclass's desinated initializer
+    self = [super init];
+    
+    // Did the superclass's designated initializer succeed?
+    if (self) {
+        // Give the instance variables initial values
+        _itemName = name;
+        _serialNumber = sNumber;
+        _valueInDollars = value;
+        
+        // Set _dateCreated to the current date and time
+        _dateCreated = [[NSDate alloc] init];
+    }
+    
+    // Return the address of the newly initialized object
+    return self;
+}
+
+- (instancetype)init
+{
+    return [self initWithItemName:@"Item"];
+}
+
+- (instancetype)initWithItemName:(NSString *)name
+{
+    return [self initWithItemName:name
+                   valueInDollars:0
+                     serialNumber:@""];
+}
+
+- (void)setItemName:(NSString *)str
+{
+    _itemName = str;
+}
+
+- (NSString *)itemName
+{
+    return _itemName;
+}
+
+- (void)setSerialNumber:(NSString *)str
+{
+    _serialNumber = str;
+}
+
+- (NSString *)serialNumber
+{
+    return _serialNumber;
+}
+
+- (void)setValueInDollars:(int)v
+{
+    _valueInDollars = v;
+}
+
+- (int)valueInDollars
+{
+    return _valueInDollars;
+}
+
+- (NSDate *)dateCreated
+{
+    return _dateCreated;
+}
+
+- (NSString *)description
+{
+    NSString *descriptionString =
+    [[NSString alloc] initWithFormat:@"%@ (%@): Worth $%d, recorded on %@",
+     self.itemName,
+     self.serialNumber,
+     self.valueInDollars,
+     self.dateCreated];
+    
+    return descriptionString;
+}
+
+@end
---
+++ RandomItems 3/RandomItems 3/main.m
@@ -0,0 +1,32 @@
+//
+//  main.m
+//  RandomItems 3
+//
+//  Created by Wei on 8/18/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BNRItem.h"
+
+int main(int argc, const char * argv[])
+{
+    
+    @autoreleasepool {
+        
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+        
+        for (int i=0; i<10; i++) {
+            BNRItem *item = [BNRItem randomItem];
+            [items addObject:item];
+        }
+        
+        for (BNRItem *item in items) {
+            NSLog(@"%@", item);
+        }
+        
+        items = nil;
+        
+    }
+    return 0;
+}
