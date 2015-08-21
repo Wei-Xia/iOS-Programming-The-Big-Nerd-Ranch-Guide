b649d8bb13318fedafe5c0c93e9c8fa339ec14a4
Wei Xia <xw900812@gmail.com>
2015-08-20 22:21:23 -0400

    Chapter 3

--- RandomItems 3/BNRItem.h
+++ RandomItems 3/BNRItem.h
@@ -19,13 +19,13 @@
 
 -(instancetype)initwithItemName:(NSString *)name;
 
-@property BNRItem *containedItem;
-@property BNRItem *container;
+@property (nonatomic, strong) BNRItem *containedItem;
+@property (nonatomic, weak) BNRItem *container;
 
-@property NSString *itemName;
-@property NSString *serialNumber;
-@property int valueInDollars;
-@property NSDate *dateCreated;
+@property (nonatomic, copy) NSString *itemName;
+@property (nonatomic, copy) NSString *serialNumber;
+@property (nonatomic) int valueInDollars;
+@property (nonatomic, readonly,strong) NSDate *dateCreated;
 
 
 @end
