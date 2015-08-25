766e79840f8a2c6c1bf9290d7553b7216281bf61
Wei Xia <xw900812@gmail.com>
2015-08-24 23:19:11 -0400

    Chapter 4

---
+++ Hypnosister/Hypnosister/BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by Wei on 8/24/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
