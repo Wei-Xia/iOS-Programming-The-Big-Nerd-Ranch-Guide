065ab35e6c38306b85c28738c63d52d1a037e42e
Wei Xia <xw900812@gmail.com>
2015-08-31 16:23:22 -0400

    Chapter 5

---
+++ Hypnosister 4/Hypnosister 4Tests/Hypnosister_4Tests.m
@@ -0,0 +1,34 @@
+//
+//  Hypnosister_4Tests.m
+//  Hypnosister 4Tests
+//
+//  Created by Wei on 8/31/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Hypnosister_4Tests : XCTestCase
+
+@end
+
+@implementation Hypnosister_4Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
